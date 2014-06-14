3079b5394fcac68b214191c002cfe28a3b25e1ca
mathieuS <mathieuS@my-pc>
2014-06-14 16:33:12 +0200

    ajout scrollingScript

---
+++ Assets/Scripts/ScrollingScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Parallax scrolling
+/// </summary>
+public class ScrollingScript : MonoBehaviour
+{
+	/// <summary>
+	/// Vitesse du défilement
+	/// </summary>
+	public Vector2 speed = new Vector2(2, 2);
+	
+	/// <summary>
+	/// Direction du défilement
+	/// </summary>
+	public Vector2 direction = new Vector2(-1, 0);
+	
+
+	
+	void Update()
+	{
+		float inputX = Input.GetAxis("Horizontal");
+		// Mouvement
+		Vector3 movement = new Vector3(
+			direction.x * speed.x * inputX,
+			speed.y * direction.y,
+			0);
+		
+		movement *= Time.deltaTime;
+		transform.Translate(movement);
+
+	}
+}
